f687c2a65f4d5c8df0a40145c80dd878d507e4bd
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2024-06-19 21:59:23 +0800

    Create grid using flexbox, update hover effect and add request input from user

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 const container = document.querySelector('#container');
-const numberBoxes = Math.pow(16,2);
+const button = document.querySelector('button');
+let input = 16;
 
-for (let i=0;i<numberBoxes;i++){
-const box = document.createElement('div');
-console.log(box)
-box.classList.add("box");
-container.appendChild(box);
+createGrid(input);
 
-box.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "black";
-})
+// function random_rgba() {
+//     var o = Math.round, r = Math.random, s = 255;
+//     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+// }
+
+
+function createGrid(input){
+
+    while (container.firstChild) {
+    container.removeChild(container.lastChild);}
+
+    const numberBoxes = input * input;
+
+    for (let i=0;i<numberBoxes;i++){
+        const box = document.createElement('div');
+        box.classList.add("box");
+        box.style.cssText = `width: ${(1 / input) * 100}%; height: ${(1 / input) * 100}%;`;
+        container.appendChild(box);
+
+        let interactions = 1;
+
+        box.addEventListener("mouseover", (event) => {
+        interactions++;
+        let opacity = interactions*0.1;
+        event.target.style.backgroundColor = `rgba(255,0,0, ${opacity})`;
+
+        if (opacity>1){
+            opacity = 1;
+        }
+        })
+    }
 }
+
+button.addEventListener('click', () => {
+
+    let inputValue = prompt('Pick the number of squares per side. Choose only from 1 to 100: ');
+    inputValue = Number(inputValue);
+    createGrid (inputValue);
+
+})
